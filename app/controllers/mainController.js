edbfef1e8079bc0bb1d1c37711b6759bc7da5416
reshma <reshma@qburst.com>
2016-07-08 12:12:33 +0530

    date adn time change

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -5,7 +5,7 @@
  	var total = 8;
  	var totalSeconds = 60;
 	// create the controller and inject Angular's $scope
-	app.controller('mainController',['$scope','loadJson','dateDropdown','saveStatus','setDateAndTime',function($scope,loadJson,dateDropdown,saveStatus,setDateAndTime) {
+	app.controller('mainController',['$scope','loadJson','dateDropdown','saveStatus',function($scope,loadJson,dateDropdown,saveStatus) {
 		loadJson.getActivityList().then(function(activityListData){
 			$scope.activityList = activityListData;
 			$scope.selectedActivity = $scope.activityList.data[0];
@@ -45,70 +45,90 @@
 
 			if($scope.dailyStatusForm.$valid) {
 
+				/** SAVING HISTORY */
 				$scope.listhistory = saveStatus.saveActivity($scope.dateSelected,
 					$scope.selectedProject,$scope.selectedActivity.value,
 					$scope.selectedMinuteSpent,$scope.selectedSecondsSpent,$scope.selectedMessage);
+				/** SAVING HISTORY */
 
+				$scope.setDate = function(key){
+					nextElemIndex = 7-key+1;
+					$scope.dateSelected = $scope.dateHTML[nextElemIndex];
+					$scope.selectedMinuteSpent = $scope.minutesDropdown[8];
+					$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
+				}
+
+				$scope.setDateAndTimeWhenDateNotToday = function(total,totalSeconds,key,sec){
+					if(total != 0 ){								
+						$scope.selectedMinuteSpent = $scope.minutesDropdown[total-1];
+						if(totalSeconds == 0){
+							$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
+							$scope.selectedMinuteSpent = $scope.minutesDropdown[8];
+							nextElemIndex = 7-key+1;
+							$scope.dateSelected = $scope.dateHTML[nextElemIndex];
+						}
+						else{
+							$scope.selectedSecondsSpent = $scope.secondsDropdown[sec];
+						}
+					}else{
+						$scope.setDate(key);
+					}
+				}
+
+				$scope.setDateAndTimeWhenDateForToday = function(total,totalSeconds,sec){
+					if(total != 0 ){								
+						$scope.selectedMinuteSpent = $scope.minutesDropdown[total-1];
+						if(totalSeconds == 0)
+							$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
+						else
+							$scope.selectedSecondsSpent = $scope.secondsDropdown[sec];
+					}else{
+						$scope.selectedMinuteSpent = $scope.minutesDropdown[8];
+						$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
+					}
+				}
+
+				/** SETTING DATE AND TIME */
 				var dateElem = angular.element(document.querySelector('#postedFor'));
 				var nextElemIndex;
 				angular.forEach($scope.dateHTML, function(data, key) {
 					if(key == $scope.dateSelected.id){
-						total = total - $scope.selectedMinuteSpent.id;
-						totalSeconds = totalSeconds - $scope.selectedSecondsSpent.id;
+						
 						if($scope.dateSelected.id != 0 && $scope.selectedMinuteSpent.id == '08'){
 							nextElemIndex = 7-key+1;
 							$scope.dateSelected = $scope.dateHTML[nextElemIndex];
 						}else if($scope.dateSelected.id != 0 && $scope.selectedMinuteSpent.id < 8){
+							if(total == 0 || total < $scope.selectedMinuteSpent.id)
+								total = 8-$scope.selectedMinuteSpent.id;
+							else
+								total = total - $scope.selectedMinuteSpent.id;
+
+							if(totalSeconds == 0 || totalSeconds < $scope.selectedSecondsSpent.id)
+								totalSeconds = 60-$scope.selectedSecondsSpent.id;
+							else
+								totalSeconds = totalSeconds - $scope.selectedSecondsSpent.id;
 							if($scope.selectedSecondsSpent.id == "00"){
-								if(total != 0 ){								
+								if(total != 0 )							
 									$scope.selectedMinuteSpent = $scope.minutesDropdown[total];
-								}else{
-									nextElemIndex = 7-key+1;
-									$scope.dateSelected = $scope.dateHTML[nextElemIndex];
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[8];
-									$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-								}
+								else
+									$scope.setDate(key);
 							}else if($scope.selectedSecondsSpent.id == "15"){
-								if(total != 0 ){								
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[total-1];
-									if(totalSeconds == 0)
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-									else
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[3];
-								}else{
-									nextElemIndex = 7-key+1;
-									$scope.dateSelected = $scope.dateHTML[nextElemIndex];
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[8];
-									$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-								}
+								$scope.setDateAndTimeWhenDateNotToday(total,totalSeconds,key,3);
 							}else if($scope.selectedSecondsSpent.id == "30"){
-								if(total != 0 ){								
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[total-1];
-									if(totalSeconds == 0)
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-									else
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[2];
-								}else{
-									nextElemIndex = 7-key+1;
-									$scope.dateSelected = $scope.dateHTML[nextElemIndex];
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[8];
-									$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-								}
+								$scope.setDateAndTimeWhenDateNotToday(total,totalSeconds,key,2);
 							}else if($scope.selectedSecondsSpent.id == "45"){
-								if(total != 0 ){								
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[total-1];
-									if(totalSeconds == 0)
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-									else
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[1];
-								}else{
-									nextElemIndex = 7-key+1;
-									$scope.dateSelected = $scope.dateHTML[nextElemIndex];
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[8];
-									$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-								}
+								$scope.setDateAndTimeWhenDateNotToday(total,totalSeconds,key,1);
 							}
 						}else if($scope.dateSelected.id == 0){
+							if(total == 0 || total < $scope.selectedMinuteSpent.id)
+								total = 8-$scope.selectedMinuteSpent.id;
+							else
+								total = total - $scope.selectedMinuteSpent.id;
+
+							if(totalSeconds == 0 || totalSeconds < $scope.selectedSecondsSpent.id)
+								totalSeconds = 60-$scope.selectedSecondsSpent.id;
+							else
+								totalSeconds = totalSeconds - $scope.selectedSecondsSpent.id;
 							if($scope.selectedSecondsSpent.id == "00"){
 								if(total != 0 ){								
 									$scope.selectedMinuteSpent = $scope.minutesDropdown[total];
@@ -117,47 +137,17 @@
 									$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
 								}
 							}else if($scope.selectedSecondsSpent.id == "15"){
-								if(total != 0 ){								
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[total-1];
-									if(totalSeconds == 0)
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-									else
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[3];
-								}else{
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[8];
-									$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-								}
+								$scope.setDateAndTimeWhenDateForToday(total,totalSeconds,3);
 							}else if($scope.selectedSecondsSpent.id == "30"){
-								if(total != 0 ){								
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[total-1];
-									if(totalSeconds == 0)
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-									else
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[2];
-								}else{
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[8];
-									$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-								}
+								$scope.setDateAndTimeWhenDateForToday(total,totalSeconds,2);
 							}else if($scope.selectedSecondsSpent.id == "45"){
-								if(total != 0 ){								
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[total-1];
-									if(totalSeconds == 0)
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-									else
-										$scope.selectedSecondsSpent = $scope.secondsDropdown[1];
-								}else{
-									$scope.selectedMinuteSpent = $scope.minutesDropdown[8];
-									$scope.selectedSecondsSpent = $scope.secondsDropdown[0];
-								}
+								$scope.setDateAndTimeWhenDateForToday(total,totalSeconds,1);
 							}
 						}
-						$scope.message = "";
 					}
+					//$scope.selectedMessage = "";
 				});
-/*		$scope.calculateTime = function(){
-			
-		}*/
-				
+			/** SETTING DATE AND TIME */				
 			}
 			else{
 				console.log("invalid");
